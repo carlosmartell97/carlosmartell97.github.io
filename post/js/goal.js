a837e3165f0955134615963c9ac8e1356bdbae09
carlosmartell97 <carlosmartell97@gmail.com>
2017-01-03 23:58:01 -0600

    previous way of displaying profile Pictures in profile.html and chat.html is wrong. It appends chats in different order because of Firebase's asyncronicity in its methods. Here, I'm trying a different approach with imageURL() function. However, this function only works for users with profilePicture in Storage, because JQuery's getJSON() is also asyncronous...

--- post/js/goal.js
+++ post/js/goal.js
@@ -75,8 +75,9 @@ var user2; var user2Found=false;
         ref.on("child_added", function(snapshot, prevChildKey) {
             var newComment = snapshot.val();
             var profilePic;
-
-            $("#Chat").append("<li class='left clearfix'><span class='chat-img pull-left'><img src='https://placehold.it/50/55C1E7/fff' alt='User Avatar' class='img-circle' /></span><div class='chat-body clearfix'><div class='header'><strong class='primary-font'>"+newComment.user+"</strong><small class='pull-right text-muted'><i class='fa fa-clock-o fa-fw'></i>"+newComment.date+"</small></div><p>"+newComment.comment+"</p></div></li>");
+            
+            $("#Chat").append("<li class='left clearfix'><span class='chat-img pull-left'><img src='"+imageURL(newComment.user)+"' alt='User Avatar' class='img-circle' style='height:55px; width:55px; display: block; object-fit:cover;'/></span><div class='chat-body clearfix'><div class='header'><strong class='primary-font'>"+newComment.user+"</strong><small class='pull-right text-muted'><i class='fa fa-clock-o fa-fw'></i>"+newComment.date+"</small></div><p>"+newComment.comment+"</p></div></li>");
+            
         });
     });
 
@@ -124,7 +125,7 @@ var newCommentFn = function(){
     document.getElementById('btn-chat').disabled=true;
 }
 var enterPress=function(e){
-    if (event.which == 13 || event.keyCode == 13) {
+    if ((event.which == 13 || event.keyCode == 13)&&(document.getElementById('comment-input').value!="")) {
     //code to execute here
     return true;
     }
@@ -137,4 +138,22 @@ var checkComment = function(){
     else{
         document.getElementById('btn-chat').disabled=false;
     }
+};
+var imageURL=function(user){
+        var token;
+        $.getJSON("https://firebasestorage.googleapis.com/v0/b/firebase-karmics.appspot.com/o/"+user+"%2FprofilePic.jpg", function( data ) {
+//            console.log(data.dataType);
+            token=data.downloadTokens;
+        }).done(function() {
+            console.log( "second success" );
+        }).fail(function() {
+            console.log( "error" );
+        });
+        if(token!=undefined){
+            return "img/profile_250x250.jpg";
+        }
+        else{
+            return "https://firebasestorage.googleapis.com/v0/b/firebase-karmics.appspot.com/o/"+user+"%2FprofilePic.jpg"+"?alt=media&token="+token;
+        }
+    
 };
